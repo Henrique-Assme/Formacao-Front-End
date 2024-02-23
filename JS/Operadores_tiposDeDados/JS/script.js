cffbf6aa15d4a260a6f855b0ce14021f5d8e20f1
Henrique Assme <heniassme@gmail.com>
2024-02-23 19:26:31 -0300

    add: operadores e tipos de dados

---
+++ JS/Operadores_tiposDeDados/JS/script.js
@@ -0,0 +1,66 @@
+// 1 - Number
+console.log(typeof 2) 
+console.log(typeof -57) 
+
+// 2 - oop log(...data/ any[]): void
+console.log(2+4)
+console.log(10+5+1)
+console.log(10*5)
+console.log(24/4)
+
+// 3 - special numbers
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(12 + "asdas")
+console.log(typeof NaN)
+
+// 4 - string
+console.log("um texto")
+console.log('Mais um texto')
+console.log(`13`)
+
+// 5 - simbolos especiais em string
+console.log("Testando a\nquebra de linha")
+console.log("Espaçamento\tde tab")
+
+// 6 - concatenação
+console.log("oi," + " tudo "+"bem?")
+
+// 7 - template string
+console.log(`A soma de 2 + 2 é ${2+2}`)
+
+// 8 - boolean
+console.log(true)
+console.log(false)
+console.log(5 == 8)
+
+// 9 - comparações
+
+console.log(5 <= 5)
+console.log(5 < 5)
+console.log(10 === 10)
+console.log(10 === 9)
+console.log(10 !== 9)
+
+// 10 - idênticos
+console.log(9 == "9")
+console.log(9 + "9")
+console.log(9 === "9")
+console.log(9 !== "9")
+
+// 11 - operadores lógicos
+console.log(true && true)
+console.log(true && false)
+console.log(5 > 2 && 2 < 10)
+console.log(5 > 2 && 'heni' === 3)
+console.log(5 > 2 || 'heni' === 3)
+console.log(!true)
+
+// 12 - empty values
+console.log(typeof null, typeof undefined)
+
+// 13 - mudança de tipos
+console.log(5*null)
+console.log("a"*"b")
+console.log("10" + 1)
+console.log("10" - 1)
